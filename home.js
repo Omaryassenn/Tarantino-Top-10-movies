312e5f60caff4435c21b50a2d59f6d27f2a70d14
Omar yassen <88896705+Omaryassenn@users.noreply.github.com>
2022-10-11 19:08:02 +0200

    7beby

---
+++ home.js
@@ -0,0 +1,104 @@
+const movies = [
+    {
+        
+        Name:"Pulp Fiction",
+        Genre:"Thriller, Crime",
+        ReleaseDate:"10/14/1994",
+        Duration:"2h 34m",
+        Overview:"A burger-loving hit man, his philosophical partner, a drug-addled gangster's moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.",
+        Director:"Quentin Tarantino",
+        Story:"Roger Avary",
+        Stars:"John Travolta ,Uma Thurman, Samuel L. Jackson",
+        Budget:"$8,000,000.00",
+        Revenue:"$214,179,088.00",
+        IMDbRATING:"8.9/10",
+    },
+    {
+        "Name":"Inglourious Basterds",
+        "Genre":"Drama, Action, Thriller, War",
+        "Release Date":"08/21/2009",
+        "Duration":"2h 33m",
+        "Overview":"In Nazi-occupied France during World War II, a group of Jewish-American soldiers known as The Basterds are chosen specifically to spread fear throughout the Third Reich by scalping and brutally killing Nazis. The Basterds, lead by Lt. Aldo Raine soon cross paths with a French-Jewish teenage girl who runs a movie theater in Paris which is targeted by the soldiers.",
+        "Director":"Quentin Tarantino",  
+        "Stars":"Brad Pitt , Diane Kruger ,Eli Roth",
+        "Budget":"$70,000,000.00",
+        "Revenue":"$321,457,747.00",
+        "IMDb RATING":"8.3/10",
+
+    },
+    {
+        "Name":"Reservoir Dogs",
+        "Genre":"Crime, Thriller",
+        "Release Date":"09/02/1992 ",
+        "Duration":"1h 39m",
+        "Overview":"A botched robbery indicates a police informant, and the pressure mounts in the aftermath at a warehouse. Crime begets violence as the survivors -- veteran Mr. White, newcomer Mr. Orange, psychopathic parolee Mr. Blonde, bickering weasel Mr. Pink and Nice Guy Eddie -- unravel.",
+        "Director":"Quentin Tarantino",
+        "Writer":"Roger Avary",
+        "Stars":"Harvey Keitel , Tim Roth , Michael Madsen",
+        "Budget":"$1,200,000.00",
+        "Revenue":"$2,859,750.00",
+        "IMDb RATING":"8.3/10",
+    },
+    {
+        "Name":"Jackie Brown",
+        "Genre":"Crime, Thriller, Drama",
+        "Release Date":"12/25/1997",
+        "Duration":"2h 34m",
+        "Overview":"Jackie Brown is a flight attendant who gets caught in the middle of smuggling cash into the country for her gunrunner boss. When the cops try to use Jackie to get to her boss, she hatches a plan — with help from a bail bondsman — to keep the money for herself.",
+        "Director":"Quentin Tarantino",
+        "Story":"Elmore Leonard",
+        "Stars":"Pam Grier , Samuel L. Jackson , Robert Forster",
+        "Budget":"$12,000,000.00",
+        "Revenue":"$39,673,162.00",
+        "IMDb RATING":"7.5/10",
+
+    },
+    {
+        "Name":"Django Unchained",
+        "Genre":"Drama, Western",
+        "Release Date":"01/16/2013",
+        "Duration":"2h 45m",
+        "Overview":"With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.",
+        "Director":"Quentin Tarantino",
+        "Stars":"Jamie Foxx , Christoph Waltz , Leonardo DiCaprio",
+        "Budget":"$100,000,000.00",
+        "Revenue":"$425,368,238.00",
+        "IMDb RATING":"8.4/10",
+    },
+    {
+        "Name":"Kill Bill: Vol. 1" ,
+        "Genre":"Action, Crime",
+        "Release Date":"12/31/2003",
+        "Duration":"1h 51m",
+        "Overview":"An assassin is shot by her ruthless employer, Bill, and other members of their assassination circle – but she lives to plot her vengeance.",
+        "Director":"Quentin Tarantino",
+        "Characters":"Uma Thurman",
+        "Stars":"Uma Thurman , David Carradine , Daryl Hannah",
+        "Budget":"$30,000,000.00",
+        "Revenue":"$180,906,076.00",
+        "IMDb RATING":"8.2/10",
+
+    },
+    {
+        "Name":"Natural Born Killers",
+        "Genre":"Crime, Thriller, Drama",
+        "Release Date":"08/25/1994",
+        "Duration":"1h 58m",
+        "Overview":"Two victims of traumatized childhoods become lovers and serial murderers irresponsibly glorified by the mass media.",
+        "Director, Screenplay":"Oliver Stone",
+        "Screenplay":"Richard Rutowski , David Veloz",
+        "Story":"Quentin Tarantino",
+        "Stars":" Woody Harrelson , Juliette Lewis , Tom Sizemore",
+        "Budget":"$34,000,000.00",
+        "Revenue":"$50,283,563.00",
+        "IMDb RATING":"7.2/10",
+    },
+    {
+
+    },
+
+      "From Dusk Till Dawn","The Hateful Eight","Four Rooms"];
+for (let i = 0; i < movies.length; i++) 
+{
+
+}
